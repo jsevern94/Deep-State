8254ddeac086160c6475193bf92bcf7c365257b0
jsevern94 <severnj@spu.edu>
2020-01-09 13:44:40 -0800

    banner completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Title from './Title';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="banner">
+        <Title />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Title.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="title">
+            <p className="slide4">d</p>
+            <p className="slide6">e</p>
+            <p className="slide1">e</p>
+            <p className="slide2">p</p>
+            <p className="slide9">s</p>
+            <p className="slide5">t</p>
+            <p className="slide3">a</p>
+            <p className="slide8">t</p>
+            <p className="slide7">e</p>
+            <p className="slide10">.</p>
+        </div>
+    );
+}
+
+export default App;
